c198a8f1f19ec345f8f9acebd542fe091335c7bd
morganvenable <venable@gmail.com>
2023-11-28 14:43:59 -0800

    both builds working, weirdness in mcuconf.h

--- keyboards/svalboard/trackball/mcuconf.h
+++ keyboards/svalboard/trackball/mcuconf.h
@@ -16,7 +16,8 @@
 
     #if (defined(POINTING_DEVICE_IS_PMW3360) || defined(POINTING_DEVICE_IS_PMW3389))
         // #ifdef USE_FFC_SPI 
-
+            // #undef RP_SPI_USE_SPI1
+            // #define RP_SPI_USE_SPI1 TRUE
         // #else
         //     #undef RP_SPI_USE_SPI0
         //     #define RP_SPI_USE_SPI0 TRUE
--- keyboards/svalboard/trackball/rules.mk
+++ keyboards/svalboard/trackball/rules.mk
@@ -1,15 +1,6 @@
-# MCU name
-MCU = RP2040
-BOOTLOADER = rp2040
-BOARD = GENERIC_RP_RP2040
+# this turns on Manna-Harbour's automousekeys:
+MH_AUTO_BUTTONS = yes
 
-CUSTOM_MATRIX = lite
-SRC += matrix.c
-
-SERIAL_DRIVER = vendor
-# this turns on trackpoint:
-#PS2_MOUSE_ENABLE = no
-# this turns on trackballs including pimoroni
 POINTING_DEVICE_ENABLE = yes
 # this selects trackball driver
 POINTING_DEVICE_DRIVER = pmw3360
@@ -27,14 +18,3 @@ endif
 ifeq ($(strip $(POINTING_DEVICE_DRIVER)), pmw3389)
 	OPT_DEFS += -DPOINTING_DEVICE_IS_PMW3389
 endif
-
-# this turns on Manna-Harbour's automousekeys:
-MH_AUTO_BUTTONS = yes
-
-ifeq ($(strip $(MH_AUTO_BUTTONS)), yes)
-  MOUSEKEY_ENABLE = yes
-  OPT_DEFS += -DMH_AUTO_BUTTONS
-endif
-
-
-
